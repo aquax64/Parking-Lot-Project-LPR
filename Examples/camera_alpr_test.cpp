9da009b0c72bbd8ac1f0f504effa6e62a7db1423
aquax64 <139064279+aquax64@users.noreply.github.com>
2025-03-11 22:49:12 -0500

    Update camera_alpr_test.cpp

--- Examples/camera_alpr_test.cpp
+++ Examples/camera_alpr_test.cpp
@@ -55,7 +55,7 @@ int main()
     // Make sure the library is loaded
     if (!openalpr.isLoaded())
     {
-        std::cerr << "Error loading OpenALPR" << endl;
+        std::cerr << "Error loading OpenALPR" << std::endl;
         return 1;
     }
 
@@ -70,13 +70,13 @@ int main()
     for (int i = 0; i < results.plates.size(); i++)
     {
         alpr::AlprPlateResult plate = results.plates[i];
-        std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << endl;
+        std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << std::endl;
 
         for (int k = 0; k < plate.topNPlates.size(); k++)
         {
             alpr::AlprPlate candidate = plate.topNPlates[k];
             std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
-            std::cout << "\t pattern_match: " << candidate.matches_template << endl;
+            std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
         }
     }
 
