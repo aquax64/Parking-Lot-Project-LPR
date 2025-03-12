ee9266ccae7ac4e9d5ae411ff8c7d834ee161a4a
aquax64 <139064279+aquax64@users.noreply.github.com>
2025-03-11 22:48:10 -0500

    Update alprtest.cpp

--- Examples/alprtest.cpp
+++ Examples/alprtest.cpp
@@ -35,7 +35,7 @@ int main()
     // Make sure the library is loaded
     if (!openalpr.isLoaded())
     {
-        std::cerr << "Error loading OpenALPR" << endl;
+        std::cerr << "Error loading OpenALPR" << std::endl;
         return 1;
     }
 
@@ -45,7 +45,7 @@ int main()
     std::cout << "Input image file... \n";
     std::string imagePath;
     std::cout << "Path: ";
-    std::getline(cin, imagePath);
+    std::getline(std::cin, imagePath);
 
     auto t1 = high_resolution_clock::now();
 
@@ -56,13 +56,13 @@ int main()
     for (int i = 0; i < results.plates.size(); i++)
     {
         alpr::AlprPlateResult plate = results.plates[i];
-        std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << endl;
+        std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << std::endl;
 
         for (int k = 0; k < plate.topNPlates.size(); k++)
         {
             alpr::AlprPlate candidate = plate.topNPlates[k];
             std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
-            std::cout << "\t pattern_match: " << candidate.matches_template << endl;
+            std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
         }
     }
 
